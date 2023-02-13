38843da4d4fd619480d1d24465f894da4f622960
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2023-02-13 11:59:51 +0800

    Refactor tooling

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,27 @@
 import { figma } from '@figmania/vite-plugin-figma'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
-export default defineConfig({
-  root: '.',
-  base: './',
-  server: { host: 'localhost', port: 8080 },
-  build: { outDir: 'build' },
-  plugins: [react(), figma({
+export default defineConfig(({ command, mode }) => {
+  const plugins: PluginOption[] = [react(), figma({
     editorType: ['figma'],
     name: 'SVG Animate',
     api: '1.0.0',
     id: '980366185319754464',
     main: 'src/main.ts'
-  }), viteSingleFile()]
+  })]
+
+  if (command === 'build') {
+    plugins.push(viteSingleFile())
+  }
+
+  return {
+    root: '.',
+    base: './',
+    server: { host: 'localhost', port: 8080 },
+    build: { outDir: 'build', minify: mode === 'production' },
+    plugins
+  }
 })
+
