2947ffc7d303a43b7ed36157b83201d9d83d9daa
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2023-03-04 13:24:14 +0800

    refactor vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,21 @@
+import { figma } from '@figmania/vite-plugin-figma'
 import react from '@vitejs/plugin-react'
 import { defineConfig, PluginOption } from 'vite'
-import { viteSingleFile } from 'vite-plugin-singlefile'
-import { figma } from './vite-plugin-figma'
 
-export default defineConfig(({ command, mode }) => {
-  const plugins: PluginOption[] = [react(), figma({
+export default defineConfig(({ command }) => {
+  const plugins: PluginOption[] = [react(), figma(command, {
     editorType: ['figma'],
     name: 'SVG Animate',
     api: '1.0.0',
     id: '980366185319754464',
-    main: 'src/main.ts',
-    esbuild: {
-      sourcemap: mode === 'production' ? false : 'inline',
-      minify: mode === 'production',
-      legalComments: 'none'
-    }
+    main: 'src/main.ts'
   })]
-
-  if (command === 'build') {
-    plugins.push(viteSingleFile())
-  }
 
   return {
     server: { host: 'localhost', port: 8080 },
     root: '.',
     base: './',
-    esbuild: {
-      legalComments: 'none'
-    },
-    build: {
-      outDir: 'build',
-      minify: mode === 'production',
-      sourcemap: mode === 'production' ? false : 'inline'
-
-    },
+    build: { outDir: 'build' },
     plugins
   }
 })
-
